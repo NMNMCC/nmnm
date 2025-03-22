5a6f78e897201a5c523c2b7d423107ccc83bb82b
intzaaa <huangyinhaow@outlook.com>
2025-03-22 21:35:22 +0800

    fix styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,12 @@
 import { AutoTextSize } from "auto-text-size";
 import HomeDescription from "./HomeDescription.mdx";
-import { useEffect, useRef, useState } from "preact/hooks";
 
 export const Home = () => {
     return (
         <div class="w-full h-full flex flex-col">
-            <div class="w-full h-full flex flex-row flex-wrap items-start bg-red-700 p-8 md:p-12 gap-8 overflow-clip">
+            <div class="w-full flex flex-row flex-wrap items-start bg-red-700 p-8 md:p-12 gap-8 overflow-clip">
                 <h1
-                    class="w-0 grow min-w-1/2 max-h-fit font-bold flex flex-col select-none text-white"
+                    class="w-0 grow min-w-3/5 font-bold flex flex-col select-none text-white"
                     style={{
                         lineHeight: "0.8",
                     }}
@@ -30,7 +29,7 @@ export const Home = () => {
                     </AutoTextSize>
                 </h1>
                 <div
-                    class={`w-full min-w-[min(24rem,_100%)] max-w-[max(24rem,_30%)] h-0 grow min-h-fit md:p-4  overflow-x-auto overflow-y-auto prose **:text-white`}
+                    class={`w-full xl:w-0 xl:grow min-h-fit md:p-4  overflow-x-clip overflow-y-auto prose **:text-white`}
                     style={{
                         scrollbarWidth: "none",
                     }}
